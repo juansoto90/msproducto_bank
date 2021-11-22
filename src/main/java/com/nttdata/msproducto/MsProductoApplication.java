02f1748ae8ec53f6f16d38952a74b5b86c0df239
Juan Soto <jsotcast@nttdata.com>
2021-11-21 19:41:46 -0500

    refactor add configuration

--- src/main/java/com/nttdata/msproducto/MsProductoApplication.java
+++ src/main/java/com/nttdata/msproducto/MsProductoApplication.java
@@ -2,12 +2,14 @@ package com.nttdata.msproducto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class MsproductoApplication {
+public class MsProductoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsproductoApplication.class, args);
+		SpringApplication.run(MsProductoApplication.class, args);
 	}
 
 }
